9478708aafb363182f6cbcc6d6ff1a099f6389fe
Mary <schmitt.mary7@gmail.com>
2018-02-06 08:33:27 -0700

    moved variables to within function scroll down

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ function textColor() {
 }
 
 
-let firstParagraph = "Squashy armchairs dirt on your nose brass scales crush the Sopophorous bean with flat side of silver dagger, releases juice better than cutting. Full moon Whomping Willow three turns should do it lemon drops. Locomotor trunks owl treats that will be 50 points, Mr. Potter. Witch Weekly, he will rise again and he will come for us, headmaster Erumpent horn. Fenrir Grayback horseless carriages ‘zis is a chance many would die for!";
 
-let firstArray = firstParagraph.split(" ");
-
-for (let i = 0; i < 5; i++) {
-    document.write(firstArray[i]);
-}
-
-document.getElementById("first").children = firstArray;
 
 function scrollDown() {
+
+    let firstParagraph = "Squashy armchairs dirt on your nose brass scales crush the Sopophorous bean with flat side of silver dagger, releases juice better than cutting. Full moon Whomping Willow three turns should do it lemon drops. Locomotor trunks owl treats that will be 50 points, Mr. Potter. Witch Weekly, he will rise again and he will come for us, headmaster Erumpent horn. Fenrir Grayback horseless carriages ‘zis is a chance many would die for!";
+
+    let firstArray = firstParagraph.split(" ");
+
+    for (let i = 0; i < 5; i++) {
+        document.write(firstArray[i]);
+    }
+
+    document.getElementById("first").innerHTML = firstArray;
 
     for (let i = 0; i < 5; i++) {
         document.removeChild(firstArray[i]);
